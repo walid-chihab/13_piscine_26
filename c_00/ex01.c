49f44bbbbe0e8aa773b7a6a3cbe95c36471fc7c4
walid-chihab <its.n.chihab@gmail.com>
2026-06-29 03:14:15 +0100

    alphabet

---
+++ c_00/ex01.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/*min maj
+*    void	ft_print_alphabet(void){
+*       char c;
+*       c =  'a';
+*       char z ;
+*       printf ("\n");
+*       while (c <= 'z'){
+*           write(1,&c,1);
+*           z=toupper(c);
+*           write(1,&z,1);
+*           c++;
+*       }
+*       printf ("\n");
+*    }
+*/
+void	ft_print_alphabet(void)
+{
+	char	c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		write(1, &c, 1);
+		c++;
+	}
+}
