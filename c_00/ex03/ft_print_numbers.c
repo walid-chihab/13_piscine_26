460148687eac053df6d0682e7e50eecb54563cde
walid-chihab <its.n.chihab@gmail.com>
2026-07-02 02:05:30 +0100

    numbers

---
+++ c_00/ex03/ft_print_numbers.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+void    ft_print_numbers()
+{
+    char    c;
+
+    c = '0';
+    while (c <= '9')
+    {
+        write(1, &c, 1);
+        write(1, "\n", 1);
+        c++;
+    }
+}
+int main(void)
+{
+    ft_print_numbers();
+    return (0);
+}
